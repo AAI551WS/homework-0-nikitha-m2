f23c8d51049a1d022201f9493feb0af652fff718
nikitha-m2 <75613310+nikitha-m2@users.noreply.github.com>
2024-01-25 10:11:48 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,25 +1,9 @@
 def hello_world():
-    """
-    Prints 'Hello, World!' to the console.
-    
-    This function does not take any arguments nor does it return any values.
-    It simply prints a greeting message to the console.
-    """
     
     # Printing the greeting message to the console
     print("Hello, World!")
 
 def add(a, b):
-    """
-    Adds two numbers and returns the result.
-
-    Parameters:
-    - a: First number to add.
-    - b: Second number to add.
-
-    Returns:
-    - The sum of `a` and `b`.
-    """
     
     # Calculating the sum of `a` and `b`
     output = a + b
